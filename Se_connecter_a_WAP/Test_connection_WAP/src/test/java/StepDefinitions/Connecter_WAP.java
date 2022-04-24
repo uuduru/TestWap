afbbad465b9742ddfae8832439557ad373c3c4b1
uuduru <102810279+uuduru@users.noreply.github.com>
2022-04-24 22:39:54 +0200

    Update Connecter_WAP.java

--- Se_connecter_a_WAP/Test_connection_WAP/src/test/java/StepDefinitions/Connecter_WAP.java
+++ Se_connecter_a_WAP/Test_connection_WAP/src/test/java/StepDefinitions/Connecter_WAP.java
@@ -11,6 +11,7 @@ public class Connecter_WAP {
 		
 @Given("Que le navigateur est ouvert")
 public void que_le_navigateur_est_ouvert() {
+	System.setProperty("webdriver.firefox.driver", "C:\\Users\\Y9G22V706\\Downloads\\Software_Ugo\\geckodriver.exe");
 	driver = new FirefoxDriver();
 }
 
