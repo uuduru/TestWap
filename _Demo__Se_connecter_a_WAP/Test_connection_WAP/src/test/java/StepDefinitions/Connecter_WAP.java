af874c2e6296d0c9358fd633494af7236df5f0aa
uuduru <102810279+uuduru@users.noreply.github.com>
2022-05-04 16:16:36 +0200

    Update Connecter_WAP.java

--- _Demo__Se_connecter_a_WAP/Test_connection_WAP/src/test/java/StepDefinitions/Connecter_WAP.java
+++ _Demo__Se_connecter_a_WAP/Test_connection_WAP/src/test/java/StepDefinitions/Connecter_WAP.java
@@ -16,7 +16,7 @@ public class Connecter_WAP {
 
 @Given("Que le navigateur est ouvert")  
 public void que_le_navigateur_est_ouvert() throws MalformedURLException {
-	 System.setProperty("webdriver.gecko.driver", "/geckodriver.exe");
+	 System.setProperty("webdriver.gecko.driver", "_Demo__Se_connecter_a_WAP/Test_connection_WAP/geckodriver.exe");
 	 driver = new FirefoxDriver();
 	//FirefoxOptions firefoxOptions = new FirefoxOptions();	
 	//WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"),firefoxOptions);
