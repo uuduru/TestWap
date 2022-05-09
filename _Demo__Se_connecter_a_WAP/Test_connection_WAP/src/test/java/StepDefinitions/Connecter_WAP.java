9abeca4c85bb9d69c3a7223420f3dd7d16b69be2
uuduru <102810279+uuduru@users.noreply.github.com>
2022-05-09 17:04:57 +0200

    Update Connecter_WAP.java

--- _Demo__Se_connecter_a_WAP/Test_connection_WAP/src/test/java/StepDefinitions/Connecter_WAP.java
+++ _Demo__Se_connecter_a_WAP/Test_connection_WAP/src/test/java/StepDefinitions/Connecter_WAP.java
@@ -4,6 +4,7 @@ import io.cucumber.java.en.*;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -13,25 +14,29 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Connecter_WAP {
+
+	WebDriver driver = null;
 	
- WebDriver driver = null;
+
 @Given("Que le navigateur est ouvert")  
 public void que_le_navigateur_est_ouvert() throws MalformedURLException {
-	 //System.setProperty("webdriver.gecko.driver", "TestWap/_Demo__Se_connecter_a_WAP/Test_connection_WAP/src/test/java/StepDefinitions/geckodriver");
-         FirefoxOptions firefoxOptions = new FirefoxOptions();	
-	 driver = new RemoteWebDriver(new URL("http://192.168.31.105:4444"),firefoxOptions);
-	 //driver = new FirefoxDriver();
+	//System.setProperty("webdriver.firefox.driver", "C:\\Users\\Y9G22V706\\Downloads\\Software_Ugo\\geckodriver.exe");
+	//driver = new FirefoxDriver();
+	FirefoxOptions firefoxOptions = new FirefoxOptions();	
+	driver = new RemoteWebDriver(new URL("http://192.168.31.105:4444"),firefoxOptions);
 }
 
 @And("Je cherche URL de WAP")
-public void je_cherche_url_de_wap() {	
+public void je_cherche_url_de_wap() {
+	
 	driver.get("https://staging.wap.ibminteractive.fr/collaborateurs");
+    
 }
 
 @And("Je rentre le nom utilisateur et son mot de passe")
 public void je_rentre_le_nom_utilisateur_et_son_mot_de_passe() throws InterruptedException {
-    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30)); 
-    wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#email")));
+	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30)); 
+	wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#email")));
     driver.findElement(By.cssSelector("#email")).sendKeys("fake.user@yopmail.com");
     driver.findElement(By.cssSelector("#password")).sendKeys("root1234");      
 }
@@ -41,5 +46,8 @@ public void je_clique_sur_le_bouton_sign_in() {
 
 	driver.findElement(By.cssSelector("#cd_login_button")).click(); 
 	driver.quit();
+    
 }
+
+
 }
