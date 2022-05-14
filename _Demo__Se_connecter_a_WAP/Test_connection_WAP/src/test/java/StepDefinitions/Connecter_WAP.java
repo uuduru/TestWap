6bc51d7e416aae09908cfe9807903a0ed9497879
uuduru <102810279+uuduru@users.noreply.github.com>
2022-05-15 00:35:44 +0200

    Update Connecter_WAP.java

--- _Demo__Se_connecter_a_WAP/Test_connection_WAP/src/test/java/StepDefinitions/Connecter_WAP.java
+++ _Demo__Se_connecter_a_WAP/Test_connection_WAP/src/test/java/StepDefinitions/Connecter_WAP.java
@@ -52,25 +52,19 @@ public void je_cherche_url_de_wap() {
 	
 	driver.get("https://staging.wap.ibminteractive.fr/collaborateurs"); 
 	//WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(1600)); 
-	//wait.until(ExpectedConditions.urlToBe("https://eu-de.appid.cloud.ibm.com/oauth/v4/0b76cff4-bd23-4434-a66d-629509764407/authorization?client_id=987d93a0-159a-4748-a8e7-611e899b0891&response_type=code&redirect_uri=https://staging.wap.ibminteractive.fr/appid/callback&scope=appid_default&language=fr&state=0azm6cWFaJezFQIFVdlnMa0G9kE="));
-	System.out.println("Ok2");
+	//wait.until(ExpectedConditions.urlToBe("
 	}
 
 /*@And("Je rentre le \"nom_utilisateur\" et son mot de passe")
 public void je_rentre_le_nom_utilisateur_et_son_mot_de_passe() throws InterruptedException {
 	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(1600)); 
-	wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#email"))).sendKeys("fake.user@yopmail.com");
-    //driver.findElement(By.cssSelector("#email")).sendKeys("fake.user@yopmail.com");
-	wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#password"))).sendKeys("root1234");
 	System.out.println("Ok3");
-    //driver.findElement(By.cssSelector("#password")).sendKeys("root1234");      
+      
 }*/
 
 @And("Je rentre le {string} et son mot de passe")
 public void je_rentre_le_et_son_mot_de_passe(String string) {
-	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(1600)); 
-	wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#email"))).sendKeys(string);
-	wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#password"))).sendKeys("root1234");
+
 	System.out.println(string);
 	
     }
